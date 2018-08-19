13dfda7a23d7b75259697070ed8aa48aa1de0a38
francoisa <andre.francois@gmail.com>
2018-08-19 10:16:13 -0400

    Add more material ui support.

--- client/components/App.js
+++ client/components/App.js
@@ -3,6 +3,19 @@ import PropTypes from 'prop-types';
 import { routerShape } from 'found/lib/PropTypes';
 import '../css/App.css';
 import {graphql, createFragmentContainer,} from 'react-relay';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import withRouter from 'found/lib/withRouter';
 
 class App extends Component {
   static propTypes = {
@@ -19,8 +32,20 @@ class App extends Component {
     super()
     this.state = {
       navigationOpen: false,
+      anchorEl: null
     }
     this.logoutLink = this.logoutLink.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+  logout() {
+    this.setState({ anchorEl: null });
+    this.props.router.push('/logout');
   }
   logoutLink(isLoggedIn) {
     console.log("App - isLoggedIn: " + isLoggedIn);
@@ -32,13 +57,50 @@ class App extends Component {
     }
   }
   render() {
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
     const { viewer, children, isLoading } = this.props;
     return (
-       <div className="App">
-          <header className="App-header">
-           <span className="App-title">User Authentication with Relay</span>
-           {this.logoutLink(viewer.isLoggedIn)}
-          </header>
+      <div className="App">
+      <AppBar position="static">
+         <Toolbar>
+           <IconButton className="menuButton" color="inherit" aria-label="Menu">
+             <MenuIcon />
+           </IconButton>
+           <Typography variant="title" color="inherit" className="flex">
+             User Authentication with Relay
+           </Typography>
+           {viewer.isLoggedIn && (
+             <div>
+               <IconButton
+                 aria-owns={open ? 'menu-appbar' : null}
+                 aria-haspopup="true"
+                 onClick={this.handleMenu}
+                 color="inherit"
+               >
+                 <AccountCircle />
+               </IconButton>
+               <Menu
+                 id="menu-appbar"
+                 anchorEl={anchorEl}
+                 anchorOrigin={{
+                   vertical: 'top',
+                   horizontal: 'right',
+                 }}
+                 transformOrigin={{
+                   vertical: 'top',
+                   horizontal: 'right',
+                 }}
+                 open={open}
+                 onClose={this.handleClose}
+               >
+                 <MenuItem href="/profile">Profile</MenuItem>
+                 <MenuItem onClick={this.logout}>Logout</MenuItem>
+               </Menu>
+             </div>
+           )}
+         </Toolbar>
+       </AppBar>
           {children}
        </div>
     );
@@ -46,7 +108,7 @@ class App extends Component {
 }
 
 export default createFragmentContainer(
-  App,
+  withRouter(App),
   graphql`
     fragment App_viewer on viewer {
       isLoggedIn
--- stories/todo.stories.js
+++ stories/todo.stories.js
@@ -5,5 +5,8 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import AddTodo from '../client/components/AddTodo';
+import { setupGraphiQL } from '@storybook/addon-graphql'
+
+const graphiql = setupGraphiQL({ url: 'http://localhost:3000/graphql' });
 
 storiesOf('AddTodo', module).add('Form', () => <AddTodo/>);
